bf919b10489e7fd7a04836f536d47375d7da05e3
Anaz S. Aji <aji.anaz@gmail.com>
2025-01-22 08:56:10 +0700

    Update : Fix bug in android samsung about flutter secure storage

--- lib/utils/micropack_storage.dart
+++ lib/utils/micropack_storage.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/services.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:micropack_core/micropack_core.dart';
 
@@ -6,7 +7,11 @@ class MicropackStorage {
 
   // Create instance dari FlutterSecureStorage
   Future<MicropackStorage> init() async {
-    _storage = const FlutterSecureStorage();
+    _storage = const FlutterSecureStorage(
+        aOptions: AndroidOptions(
+      resetOnError: true,
+      encryptedSharedPreferences: true,
+    ));
     return this;
   }
 
@@ -17,9 +22,24 @@ class MicropackStorage {
   }
 
   static Future<String?> read({required String key}) async {
-    final s = await _storage.read(key: key);
-    logSys("Load $key : $s");
-    return s;
+    try {
+      final s = await _storage.read(key: key);
+      logSys("Load $key : $s");
+      return s;
+    } on PlatformException catch (e) {
+      logSys("Error reading key $key: ${e.message}");
+
+      // Workaround for https://github.com/mogol/flutter_secure_storage/issues/43
+      if (key == "token") {
+        await _storage.deleteAll();
+        logSys("All secure storage data deleted due to token key error.");
+      }
+
+      return null; // Explicitly return null in case of an error
+    } catch (e) {
+      logSys("Unexpected error while reading key $key: $e");
+      return null; // Handle any other unexpected errors
+    }
   }
 
   static Future<void> delete({required String key}) async {
